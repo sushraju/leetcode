9b2ca8800ab99a0ddeff8b74e289904749afe954
Suresh Raju <sushraju@gmail.com>
2020-04-02 11:05:13 -0700

    pylint edits

--- hash_file_contents.py
+++ hash_file_contents.py
@@ -1,5 +1,5 @@
 """
-# This script builds a dictionary of identical files in a directory (and nested contents)
+Builds a dictionary of identical files in a directory (and nested contents)
 """
 # !/usr/bin/env python
 import os
@@ -10,8 +10,8 @@ from collections import defaultdict
 
 class FileNameHash():
     """
-    # Class builds a dictionary of using the hash(file's contents) as key
-    # and the filename as a list of values.
+    Class builds a dictionary of using the hash(file's contents) as key
+    and the filename as a list of values.
     """
     def __init__(self, path):
         self.path = path
@@ -20,9 +20,11 @@ class FileNameHash():
         self.find_files(self.path)
         self.build_file_names_hash()
 
-    # Find the files in a directory.  The worst case complexity is O(N^2), which is not great.
     def find_files(self, path):
-        """Traverse the directory of files and other directories recursively and build a list"""
+        """
+        Traverse the directory of files and other directories recursively and build a list
+        Find the files in a directory.  The worst case complexity is O(N^2), which is not great.
+        """
 
         if self.all_files is None:
             self.all_files = []
