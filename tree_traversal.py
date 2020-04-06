6d3bce220616a2d78ba1e85e0f2a3341b8695f40
Suresh Raju <sushraju@gmail.com>
2020-04-06 15:41:18 -0700

    refactor and pylint edits

--- tree_traversal.py
+++ tree_traversal.py
@@ -1,69 +1,94 @@
-#!/usr/bin/env python
+"""BST construction and traversal"""
+
+
+# !/usr/bin/env python
 
 class Node:
+    """Represents a node in a tree with left and right subtrees"""
 
-    def __init__(self,key):
+    def __init__(self, key):
+        """Initialize the node"""
         self.left = None
         self.right = None
         self.val = key
 
 
-# A function to do inorder tree traversal
-def printInorder(root):
+def insert_bst(root, node):
+    """Function to build a binary search tree"""
+    if root is None:
+        root = node
+    else:
+        if root.val < node.val:
+            if root.right is None:
+                root.right = node
+            else:
+                insert_bst(root.right, node)
+        else:
+            if root.left is None:
+                root.left = node
+            else:
+                insert_bst(root.left, node)
+
+
+def print_in_order(root):
+    """A function to do inorder tree traversal"""
     if root:
         # First recur on left child
-        printInorder(root.left)
+        print_in_order(root.left)
 
         # then print the data of node
-        print(root.val),
+        print(root.val)
 
         # now recur on right child
-        printInorder(root.right)
+        print_in_order(root.right)
 
 
-# A function to do postorder tree traversal
-def printPostorder(root):
+def print_post_order(root):
+    """A function to do postorder tree traversal"""
     if root:
         # First recur on left child
-        printPostorder(root.left)
+        print_post_order(root.left)
 
         # the recur on right child
-        printPostorder(root.right)
+        print_post_order(root.right)
 
         # now print the data of node
-        print(root.val),
+        print(root.val)
 
 
-# A function to do preorder tree traversal
-def printPreorder(root):
+def print_pre_order(root):
+    """A function to do preorder tree traversal"""
     if root:
         # First print the data of node
-        print(root.val),
+        print(root.val)
 
         # Then recur on left child
-        printPreorder(root.left)
+        print_pre_order(root.left)
 
         # Finally recur on right child
-        printPreorder(root.right)
+        print_pre_order(root.right)
+
 
 def main():
-    # Driver code
+    """Driver code"""
     root = Node(5)
-    root.left = Node(3)
-    root.right = Node(7)
-    root.left.left = Node(2)
-    root.left.right = Node(4)
-    root.left.left.left = Node(1)
-    root.right.left = Node(6)
-    root.right.right = Node(8)
-    print ("Preorder traversal of binary tree is")
-    printPreorder(root)
+    insert_bst(root, Node(3))
+    insert_bst(root, Node(7))
+    insert_bst(root, Node(2))
+    insert_bst(root, Node(4))
+    insert_bst(root, Node(1))
+    insert_bst(root, Node(6))
+    insert_bst(root, Node(8))
 
-    print ("\nInorder traversal of binary tree is")
-    printInorder(root)
+    print("Preorder traversal of binary tree is")
+    print_pre_order(root)
 
-    print ("\nPostorder traversal of binary tree is")
-    printPostorder(root)
+    print("\nInorder traversal of binary tree is")
+    print_in_order(root)
+
+    print("\nPostorder traversal of binary tree is")
+    print_post_order(root)
+
 
 if __name__ == "__main__":
-   main()
+    main()
