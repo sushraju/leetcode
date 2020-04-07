14c62d551021cd2d144bb30b09642e0a56636f6f
Suresh Raju <sushraju@gmail.com>
2020-04-07 15:55:14 -0700

    edits

--- log_rotator.py
+++ log_rotator.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 
 def log_restrict(source_fd, max_files, max_size, prefix):
+    """Reads and writes to log files based on max bytes read"""
     line = source_fd.readline(max_size)
-    linesize = 0
+    line_size = 0
     fn_ctr = 1
     output_file = './' + 'prefix' + '_' + str(fn_ctr) + '.log'
     op = open(output_file, 'a')
     op.truncate(0)
     while line:
-        linesize = linesize + len(line)
-        #print(linesize)
-        if linesize <= max_size:
+        line_size = line_size + len(line)
+
+        if line_size <= max_size:
             op.write(line)
         else:
             op.close()
-
             if fn_ctr < max_files:
                 fn_ctr = fn_ctr + 1
             else:
@@ -24,8 +24,8 @@ def log_restrict(source_fd, max_files, max_size, prefix):
             op = open(output_file, 'a')
             op.truncate(0)
             op.write(line)
-            linesize = 0
-	
+            line_size = 0
+
         line = source_fd.readline(max_size)
 
 
