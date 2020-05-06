7bfd95c95dbb08eb5f9471b0668d843503fe3729
Suresh Raju <sushraju@gmail.com>
2020-05-06 07:18:44 -0700

    first dupe, borrowed algo

---
+++ first_dupe.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+
+def first_duplicate(my_list):
+    for index in my_list:
+        print(my_list[abs(index) - 1])
+        if my_list[abs(index) - 1] < 0:
+            return abs(index)
+        else:
+            my_list[abs(index) - 1] = my_list[abs(index) - 1] * -1
+    return -1
+
+
+def main():
+    """
+    Driver code
+    :return:
+    """
+    nums = [1, 2, 3, 3, 2, 1]
+    print(first_duplicate(nums))
+
+
+if __name__ == "__main__":
+    main()
