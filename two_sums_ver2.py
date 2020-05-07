b3c55a24ac380b55a24a5eef5b54093c37d06226
Suresh Raju <sushraju@gmail.com>
2020-05-07 07:40:55 -0700

    two sums ver 2

---
+++ two_sums_ver2.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+"""
+Two sums - find pairs in a list that add up to a target sum
+"""
+
+
+class TwoSum():
+    """
+    Two sums - find pairs in a list that add up to a target sum
+    """
+
+    def __init__(self, nums):
+        self.nums = nums
+
+    def two_sum(self, target):
+        """
+        This uses a dictionary for a lookup and uses a set to avoid duplication of pairs.
+        :param target: target sum
+        :return: a set of pairs
+        """
+        lookup_d = {}
+        pairs = set()
+        for i in self.nums:
+
+            if str(target - i) not in lookup_d:
+                pairs.add((i, (target - i)))
+
+            lookup_d[str(i)] = i
+
+        return pairs
+
+
+def main():
+    """
+    Driver code
+    :return:
+    """
+    nums = [7, 3, 4, 5, 2, 2, 7, 9, 9]
+    target = 11
+
+    print(TwoSum(nums).two_sum(target))
+
+
+if __name__ == "__main__":
+    main()
